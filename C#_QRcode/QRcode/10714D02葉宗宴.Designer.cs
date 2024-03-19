a03a8f827e8e4e4db3689d6d8273cde79eec6729
yehchungyenmi <163502359+yehchungyenmi@users.noreply.github.com>
2024-03-19 10:33:13 +0800

    Add files via upload

---
+++ C#_QRcode/QRcode/10714D02葉宗宴.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace QRcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_decode = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(466, 496);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "encode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 415);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(484, 75);
+            this.textBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(57, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(484, 396);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_decode
+            // 
+            this.button_decode.Location = new System.Drawing.Point(1036, 496);
+            this.button_decode.Name = "button_decode";
+            this.button_decode.Size = new System.Drawing.Size(75, 23);
+            this.button_decode.TabIndex = 0;
+            this.button_decode.Text = "decode";
+            this.button_decode.UseVisualStyleBackColor = true;
+            this.button_decode.Click += new System.EventHandler(this.button_decode_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(627, 415);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(484, 75);
+            this.textBox2.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(627, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(484, 396);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1244, 558);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_decode);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "10714D025葉宗宴";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_decode;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ C#_QRcode/QRcode/10714D02葉宗宴.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+using ZXing;
+using System.IO;
+
+namespace QRcode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Bitmap bitmap = null;
+            string content = textBox1.Text;
+            try
+            {
+                ZXing.BarcodeWriter writer = new ZXing.BarcodeWriter
+                {
+                    Format = ZXing.BarcodeFormat.QR_CODE,
+                    Options = new ZXing.QrCode.QrCodeEncodingOptions
+                    {
+                        Height = 180,
+                        Width = 180,
+                        CharacterSet = "UTF-8",
+                        ErrorCorrection = ZXing.QrCode.Internal.ErrorCorrectionLevel.H
+                    }
+                };
+                bitmap = writer.Write(content);
+                bitmap.Save("D:\\Test\\temp.png", System.Drawing.Imaging.ImageFormat.Png);
+                pictureBox1.Image = bitmap;
+            }
+            catch (Exception EX)
+            {
+                MessageBox.Show("Something Is Wrong !" + EX.ToString());
+            }
+
+
+        }
+
+        private void button_decode_Click(object sender, EventArgs e)
+        {
+            
+            System.Drawing.Bitmap bitmap = null;
+   
+            ZXing.IBarcodeReader reader = new ZXing.BarcodeReader();
+
+
+            FileStream fs = new FileStream("D:\\Test\\temp.png", FileMode.Open);
+            Byte[] data = new Byte[fs.Length];       
+            fs.Read(data, 0, data.Length);
+            fs.Close();
+            MemoryStream ms = new MemoryStream(data);
+            bitmap = (Bitmap)Image.FromStream(ms);
+            pictureBox2.Image = bitmap;
+            ZXing.Result result = reader.Decode(bitmap);
+            if (result != null)
+           
+                textBox2.Text = result.Text;
+            }
+        }
+
+
+    }
+
+
